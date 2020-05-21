6c4100d45213eff2036a0c8a4362b13109b99ff1
Arthur Khutak <arty4prof@gmail.com>
2020-05-21 18:35:27 +0300

    Setup simple PCA with rendering in View (#16)

--- LSA/Controllers/DataAnalysisController.cs
+++ LSA/Controllers/DataAnalysisController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Accord.IO;
+using Accord.Statistics.Analysis;
 using LSA.Data;
 using LSA.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LSA.Controllers
 {
@@ -44,6 +47,74 @@ namespace LSA.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        // GET: DataAnalysis/PCA_Analysis/5
+        public async Task<IActionResult> PCA_Analysis(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dataAnalysis = await _context.FilesInformation.FindAsync(id);
+            if (dataAnalysis == null)
+            {
+                return NotFound();
+            }
+
+            string path = await _context.FilesInformation.Where(m => m.Id == id).Select(d => d.Path).FirstOrDefaultAsync();
+
+            CsvReader reader = new CsvReader(_appEnvironment.WebRootPath + path, hasHeaders: true);
+            double[][] actual = reader.ToJagged();
+
+            var pcaTool = new KernelPrincipalComponentAnalysis();
+            pcaTool.Learn(actual);
+            pcaTool.NumberOfOutputs = 2;
+            var outputMatrix = pcaTool.Transform(actual);
+
+            ViewBag.data = outputMatrix;
+            ViewBag.columns = pcaTool.NumberOfOutputs;
+            ViewBag.rows = outputMatrix.Length;
+
+            return View();
+        }
+
+        // GET: DataAnalysis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dataAnalysis = await _context.FilesInformation
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dataAnalysis == null)
+            {
+                return NotFound();
+            }
+
+            return View(dataAnalysis);
+        }
+
+        // POST: DataAnalysis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dataAnalysis = await _context.FilesInformation.FindAsync(id);
+            _context.FilesInformation.Remove(dataAnalysis);
+
+            string path = await _context.FilesInformation.Where(m => m.Id == id).Select(d => d.Path).FirstOrDefaultAsync();
+
+            if (System.IO.File.Exists(_appEnvironment.WebRootPath + path))
+            {
+                System.IO.File.Delete(_appEnvironment.WebRootPath + path);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
